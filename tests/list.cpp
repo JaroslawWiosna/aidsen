10309d9f2f440b527a6fac8ff2edff9d242443fb
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2021-03-12 21:32:45 +0100

    Extract `struct List` to new patch

--- tests/list.cpp
+++ tests/list.cpp
@@ -2,53 +2,6 @@
 
 using namespace aidsen;
 
-template <typename Item>
-struct List {
-    struct Node {
-        Item item{};
-        Node *next{};
-        Node *prev{};
-    };
-    Node *head{};
-    Node *tail{};
-
-    void print() {
-        Node *curr{head};
-        while (curr != nullptr) {
-            ::aidsen::print(stdout, curr->item, ' ');
-            curr = curr->next;
-        }
-        println(stdout);
-    }
-
-    void push_back(Item that) {
-        if (nullptr == head && nullptr == tail) {
-            head = (Node*)malloc(sizeof(Node));
-            head->item = that;
-            tail = head;
-        } else {
-            assert(nullptr == tail->next);
-            tail->next = (Node*)malloc(sizeof(Node));
-            tail->next->item = that;
-            tail->next->prev = tail;
-            tail = tail->next;
-        }
-    }
-    void push_front(Item that) {
-        if (nullptr == head && nullptr == tail) {
-            head = (Node*)malloc(sizeof(Node));
-            head->item = that;
-            tail = head;
-        } else {
-            assert(nullptr == head->prev);
-            head->prev = (Node*)malloc(sizeof(Node));
-            head->prev->item = that;
-            head->prev->next = head;
-            head = head->prev;
-        }
-    }
-};
-
 int main() {
     List<int> list;
     list.push_back(42);
