6b68214001d72b76e73455dc7860399438e24582
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2021-03-12 21:15:26 +0100

    Fix `list`

--- tests/list.cpp
+++ tests/list.cpp
@@ -10,26 +10,43 @@ struct List {
     };
     Node *node{};
 
-    void push(Item that) {
+    void print() {
+        Node *curr{node};
+        while (curr != nullptr) {
+            ::aidsen::print(stdout, curr->item, ' ');
+            curr = curr->next;
+        }
+        println(stdout);
+    }
+
+    void push_back(Item that) {
         if (nullptr == node) {
             node = (Node*)malloc(sizeof(Node));
             node->item = that;
+            println(stdout, "\tnode->item == ", node->item);
         } else {
-            Node *curr{};
-            curr = node->next;
-            while (nullptr != curr) {
+            Node *curr = node->next;
+            if (nullptr == curr) {
+                node->next = (Node*)malloc(sizeof(Node));
+                node->next->item = that;
+                return;
+            }
+            while (nullptr != curr->next) {
                 curr = curr->next;
             }
-            curr = (Node*)malloc(sizeof(Node));
-            curr->item = that;
+            curr->next = (Node*)malloc(sizeof(Node));
+            curr->next->item = that;
         }
     }
 };
 
 int main() {
     List<int> list;
-    list.push(42);
-    list.push(43);
+    list.push_back(42);
+    list.push_back(43);
+    list.push_back(44);
+    list.push_back(45);
+    list.print();
 
     return 0;
 }
