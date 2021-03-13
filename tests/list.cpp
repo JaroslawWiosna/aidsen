edf48efc55cea038f1c3167c6e0ec3956cc5dd5b
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2021-03-13 18:43:55 +0100

    List::insert(...)

--- tests/list.cpp
+++ tests/list.cpp
@@ -8,11 +8,27 @@ int main() {
     list.push_back(43);
     list.push_back(44);
     list.push_back(45);
+
     list.print();
+    println(stdout, "size == ", list.size());
+    println(stdout, "1st  == ", list.at(0)->item);
+
+    println(stdout);
 
     list.push_front(41);
     list.push_front(40);
+
     list.print();
+    println(stdout, "size == ", list.size());
+    println(stdout, "1st  == ", list.at(0)->item);
+
+    auto item_4th = list.at(3);
+    assert(43 == item_4th->item);
+    list.insert(3, {10});
+
+    list.print();
+    println(stdout, "size == ", list.size());
+    println(stdout, "1st  == ", list.at(0)->item);
 
     return 0;
 }
