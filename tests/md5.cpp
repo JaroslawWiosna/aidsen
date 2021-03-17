eddd419ac3de0038e5134b1b7bc8d841242b8843
Jarosław Wiosna <jaroslaw.wiosna.github@gmail.com>
2021-03-17 22:23:54 +0100

    More helper function for `md5`

--- tests/md5.cpp
+++ tests/md5.cpp
@@ -3,7 +3,7 @@
 using namespace aidsen;
 
 // rfc1321
-void md5(String_View in, String_Buffer *out) {
+void md5(const String_View in, String_Buffer *out) {
     constexpr uint32_t A = 0x67452301;
     constexpr uint32_t B = 0xefcdab89;
     constexpr uint32_t C = 0x98badcfe;
@@ -18,6 +18,43 @@ void md5(String_View in, String_Buffer *out) {
     constexpr auto G = [](uint32_t X, uint32_t Y, uint32_t Z){return (X & Z) | (Y & (~Z));};  
     constexpr auto H = [](uint32_t X, uint32_t Y, uint32_t Z){return X ^ Y ^ Z;};  
     constexpr auto I = [](uint32_t X, uint32_t Y, uint32_t Z){return Y ^ (X | (~Z));};  
+
+    using word = uint32_t;
+    using uint8 = char;
+    Dynamic_Array<uint8> data{};
+    for (int i{}; i<in.count; ++i) {
+        data.push(in.data[i]);
+    }
+    data.push(1 << 7);
+    while (448 != mod((int)data.size, 512)) {
+        data.push(0);
+    }
+    data.push((int)data.size);
+    println(stdout, "data.size == ", data.size);
+    uint8 block[64]{};
+
+#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+
+#define FF(a, b, c, d, x, s, ac) { \
+     (a) += F ((b), (c), (d)) + (x) + (UINT4)(ac); \
+     (a) = ROTATE_LEFT ((a), (s)); \
+ (a) += (b); \
+  }
+#define GG(a, b, c, d, x, s, ac) { \
+ (a) += G ((b), (c), (d)) + (x) + (UINT4)(ac); \
+ (a) = ROTATE_LEFT ((a), (s)); \
+ (a) += (b); \
+  }
+#define HH(a, b, c, d, x, s, ac) { \
+ (a) += H ((b), (c), (d)) + (x) + (UINT4)(ac); \
+ (a) = ROTATE_LEFT ((a), (s)); \
+ (a) += (b); \
+  }
+#define II(a, b, c, d, x, s, ac) { \
+ (a) += I ((b), (c), (d)) + (x) + (UINT4)(ac); \
+ (a) = ROTATE_LEFT ((a), (s)); \
+ (a) += (b); \
+  }    
 
     panic("Not implemented. Yet.");
 }
@@ -40,5 +77,10 @@ int main() {
         String_Buffer sb_out{sizeof(output_buf), output_buf};
         md5(""_sv, &sb_out);
         assert("d41d8cd98f00b204e9800998ecf8427e"_sv == sb_out.view());
+    }
+    {
+        char output_buf[32];
+        String_Buffer sb_out{sizeof(output_buf), output_buf};
+        md5("verylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessagverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessagverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageeeverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessageverylongmessage"_sv, &sb_out);
     }
 }
